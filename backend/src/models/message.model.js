220b4394ea8ffb0177516887494f148b1bf3d4b1
Ranjan Vernekar <126013284+realranjan@users.noreply.github.com>
2026-08-25 20:02:38 +0530

    Exports message router and cleans up model import
    Exports the message routes to make them accessible for use by the main application. Also removes an unused `User` model import from the message schema file, improving code hygiene.

--- backend/src/models/message.model.js
+++ backend/src/models/message.model.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-import User from "./user.model";
+
 const messageSchema = new mongoose.Schema(
   {
     senderId: {
--- backend/src/routes/message.routes.js
+++ backend/src/routes/message.routes.js
@@ -2,13 +2,17 @@ import express from "express";
 import { upload } from "../middleware/upload.middleware.js";
 import {
   getUSersForSidebar,
-  getMessages,sendMessage
+  getMessages,
+  sendMessage,
 } from "../controllers/message.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
+
 const router = express.Router();
 router.use(protectRoute); // instead of router.get("/users", protectRoute, getUSersForSidebar);
 router.get("/users", getUSersForSidebar);
 router.get("/conversations", getUSersForSidebar);
 router.get("/:id", getMessages);
-router.post("/send/:id", upload.single("media") ,sendMessage);
+router.post("/send/:id", upload.single("media"), sendMessage);
 //todo:show this in the frontend part
+
+export default router;
